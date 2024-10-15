5402916de016254fa5be8c0fd1aaeda998a0ea58
Jony Reyes <151489444+JonRe88@users.noreply.github.com>
2024-10-15 14:25:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-window.addEventListener("scroll", function () {
-  var scrolled = window.pageYOffset;
-  var parallax = document.querySelector(".parallax-bg");
-  parallax.style.transform = "translateY(" + scrolled * 0.5 + "px)";
-});
+async function changeWords(params) {
+  const words = ["tecnología", "procesos", "energía"];
+  let index = 0;
+  const changingWordElement = document.getElementById("changing-word");
+
+  function changeWord() {
+    // Desvanece la palabra actual
+    changingWordElement.style.opacity = 0;
+
+    setTimeout(() => {
+      // Cambia la palabra después de que se desvanezca
+      changingWordElement.textContent = words[index];
+      index = (index + 1) % words.length;
+
+      // Aparece la nueva palabra
+      changingWordElement.style.opacity = 1;
+    }, 1000); // 1 segundo para que se desvanezca completamente
+  }
+
+  // Cambia la palabra cada 3 segundos, permitiendo que se disuelva y aparezca
+  setInterval(() => {
+    changeWord();
+  }, 3000); // Cambiar palabra cada 3 segundos
+}
+
+// Invocar la función
+changeWords();
